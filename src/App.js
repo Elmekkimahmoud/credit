0221b7bc95fc664c82721964601ed417f44d9de1
Elmekkimahmoud <59558915+Elmekkimahmoud@users.noreply.github.com>
2020-02-11 11:19:12 +0100

    Create a React app and publish it to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Credit from './credit-card';
+
+function App() {
+  return (
+    
+    <Credit/>
+  );
+}
+
+export default App;
